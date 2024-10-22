c28519771fc6370b4f3122888998a553991d2389
Yasuhiro Yamada <yamada@gr3.ie>
2024-10-22 22:02:28 +0900

    Support multiple records

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def main():
     config = load_config()
     api_token = config['cloudflare']['api_token']
     zone_name = config['cloudflare']['zone_name']
-    record_name = config['cloudflare']['record_name']
+    record_names = config['cloudflare']['record_names']
     interface = config['network']['interface']
 
     local_ip = get_local_ip(interface)
@@ -101,10 +101,11 @@ def main():
         print(f"Local IP: {local_ip}")
         zone_id = get_zone_id(api_token, zone_name)
         if zone_id:
-            record_id = get_record_id(api_token, zone_id, record_name)
-            if record_id:
-                if update_record(api_token, zone_id, record_id, local_ip, record_name):
-                    write_cached_ip(local_ip)
+            for record_name in record_names:
+                record_id = get_record_id(api_token, zone_id, record_name)
+                if record_id:
+                    if update_record(api_token, zone_id, record_id, local_ip, record_name):
+                        write_cached_ip(local_ip)
 
 if __name__ == "__main__":
     main()
